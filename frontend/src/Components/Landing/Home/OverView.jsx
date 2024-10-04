c32b50df6718ec17a77b3959b2670c316368d858
Nightingale0007 <qilicot@gmail.com>
2024-10-04 13:32:00 +0800

    Login and sign up self-adaption real numbers of covid-19

--- frontend/src/Components/Landing/Home/OverView.jsx
+++ frontend/src/Components/Landing/Home/OverView.jsx
@@ -1,41 +1,61 @@
 "use client";
-import OverView1 from '../../../assets/overview/overview1.jpg'
-import OverView2 from '../../../assets/overview/overview2.jpg'
-import OverView3 from '../../../assets/overview/overview3.jpg'
-import React from "react";
+import OverView1 from '../../../assets/overview/overview1.jpg';
+import OverView2 from '../../../assets/overview/overview2.jpg';
+import OverView3 from '../../../assets/overview/overview3.jpg';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function OverView() {
+    const [countries, setCountries] = useState(null);
+    const [today, setToday] = useState(null);
+    const [death, setDeath] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get("https://disease.sh/v3/covid-19/all");
+                setToday(response.data.cases); // 设置今天的病例数
+                setCountries(response.data.affectedCountries); // 设置受影响的国家数
+                setDeath(response.data.deaths); // 设置今天的死亡人数
+            } catch (error) {
+                console.log("Error fetching data:", error);
+            }
+        };
+
+        fetchData(); // 调用请求函数
+    }, []);
+
     return (
         <div className='flex justify-center'>
             <div className='w-11/12 h-full'>
-                    <p className='text-xl text-cyan-400 ml-4'>OVERVIEW</p>
-                    <br/>
-                    <p className="font-bold text-6xl ml-4">COVID-19 at a Glance</p>
-                    <br/>
-                    <br/>
+                <p className='text-xl text-cyan-400 ml-4'>OVERVIEW</p>
+                <br />
+                <p className="font-bold text-6xl ml-4">COVID-19 at a Glance</p>
+                <br />
+                <br />
                 <div className='flex md:flex-row flex-col justify-center items-center'>
                     <div className='flex flex-col items-center my-4 w-full md:w-1/3 h-full'>
-                        <img src={OverView1}/>
-                        <br/>
-                        <p className='text-4xl'>231</p>
+                        <img src={OverView1} alt="Overview 1" />
+                        <br />
+                        <p className='text-4xl'>{countries}</p>
                         <p className='text-xl'>Affected Countries</p>
                     </div>
                     <div className='flex flex-col items-center my-4 w-full md:w-1/3 h-full'>
-                        <img src={OverView2}/>
-                        <br/>
-                        <p className='text-4xl'>3959</p>
-                        <p className='text-xl'>Cases Today</p>
+                        <img src={OverView2} alt="Overview 2" />
+                        <br />
+                        <p className='text-4xl'>{today}</p>
+                        <p className='text-xl'>Cases Total</p>
                     </div>
                     <div className='flex flex-col items-center my-4 w-full md:w-1/3 h-full'>
-                        <img src={OverView3}/>
-                        <br/>
-                        <p className='text-4xl'>57</p>
-                        <p className='text-xl'>Deaths Today</p>
+                        <img src={OverView3} alt="Overview 3" />
+                        <br />
+                        <p className='text-4xl'>{death}</p>
+                        <p className='text-xl'>Deaths Total</p>
                     </div>
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
-export default OverView
+export default OverView;
